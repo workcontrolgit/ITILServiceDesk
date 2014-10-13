ba0771b2f40743cf12f249aa91510b92d9337e27
fujinguyen <fuji.nguyen@live.com>
2014-10-13 08:42:05 -0400

    wap and bootstrap

--- Components/FeatureController.cs
+++ Components/FeatureController.cs
@@ -1,5 +1,5 @@
 /*
-' Copyright (c) 2014 Aspose.com
+' Copyright (c) 2014 
 '  All rights reserved.
 ' 
 ' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
